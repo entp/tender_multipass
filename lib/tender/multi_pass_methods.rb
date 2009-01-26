72ab74fcefb9902bb089a61d0c3101ce261df6ba
Xavier Shay <xavier@rhnh.net>
2009-01-27 07:50:21 +0800

    Provide a method for logging out of tender
    Signed-off-by: entp <rick@entp.com>

--- lib/tender/multi_pass_methods.rb
+++ lib/tender/multi_pass_methods.rb
@@ -3,5 +3,9 @@ module Tender
     def tender_multipass(cookies, expires = nil)
       Tender::MultiPass.new(self).create(cookies, expires)
     end
+
+    def tender_expire(cookies)
+      Tender::MultiPass.new(self).expire(cookies)
+    end
   end
 end
