95d692db883d22a8bbd33460e1ca5fa80020a4b7
Xavier Shay <xavier@rhnh.net>
2009-01-26 15:50:21 -0800

    Provide a method for logging out of tender

--- test/tender_multipass_test.rb
+++ test/tender_multipass_test.rb
@@ -8,6 +8,15 @@ module Tender
   MultiPass.site_key       = "abc"
   MultiPass.support_domain = "help.xoo.com"
   MultiPass.cookie_domain  = ".xoo.com"
+
+  class TestCookieJar < Hash
+    attr_reader :deleted_keys
+
+    def delete(key, opts = {})
+      @deleted_keys ||= {}
+      @deleted_keys[key] = opts
+    end
+  end
 end
 
 class TenderMultipassTest < Test::Unit::TestCase
@@ -29,5 +38,25 @@ class TenderMultipassTest < Test::Unit::TestCase
     digest = OpenSSL::Digest::Digest.new("SHA1")
     hash   = OpenSSL::HMAC.hexdigest(digest, Tender::MultiPass.site_key, "#{Tender::MultiPass.support_domain}/#{@user.email}/1234")
     assert_equal @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
+  end
+end
+
+class TenderExpireTest < Test::Unit::TestCase
+  def setup
+    @user    = Tender::TestUser.new("seaguy@hero.com")
+    @cookies = Tender::TestCookieJar.new 
+    @user.tender_expire(@cookies)
+  end
+
+  def test_tender_email_cookie_is_cleared
+    assert_equal @cookies.deleted_keys[:tender_email], :domain => Tender::MultiPass.cookie_domain
+  end
+
+  def test_tender_expires_cookie_is_cleared
+    assert_equal @cookies.deleted_keys[:tender_expires], :domain => Tender::MultiPass.cookie_domain
+  end
+
+  def test_tender_hash_cookie_is_eaten
+    assert_equal @cookies.deleted_keys[:tender_hash], :domain => Tender::MultiPass.cookie_domain
   end
 end
