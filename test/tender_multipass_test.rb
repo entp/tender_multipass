a015e2614518814ee1c67dab282771bac9ff0dcc
rick <technoweenie@gmail.com>
2009-01-30 10:51:19 -0800

    allow custom tender options

--- test/tender_multipass_test.rb
+++ test/tender_multipass_test.rb
@@ -31,3 +31,29 @@ class TenderMultipassTest < Test::Unit::TestCase
     assert_equal @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
   end
 end
+
+class TenderMultipassWithOptionsTest < Test::Unit::TestCase
+  def setup
+    @user    = Tender::TestUser.new("seaguy@hero.com")
+    @cookies = {}
+    @user.tender_multipass(@cookies, :expires => 1234, :foo => 'bar')
+  end
+
+  def test_custom_tender_cookie_is_set
+    assert_equal @cookies[:tender_foo], :value => 'bar', :domain => Tender::MultiPass.cookie_domain
+  end
+
+  def test_tender_email_cookie_is_set
+    assert_equal @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
+  end
+
+  def test_tender_expires_cookie_is_set
+    assert_equal @cookies[:tender_expires], :value => "1234", :domain => Tender::MultiPass.cookie_domain
+  end
+
+  def test_tender_hash_cookie_is_set
+    digest = OpenSSL::Digest::Digest.new("SHA1")
+    hash   = OpenSSL::HMAC.hexdigest(digest, Tender::MultiPass.site_key, "#{Tender::MultiPass.support_domain}/#{@user.email}/1234")
+    assert_equal @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require 'rubygems'
 require 'test/unit'
+require 'active_support'
 require 'tender/multi_pass'
 require 'tender/multi_pass_methods'
