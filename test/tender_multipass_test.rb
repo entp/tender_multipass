83ef27f695b90903a75542d71fb26d15e5963f85
Xavier Shay <xavier@rhnh.net>
2009-02-06 10:49:59 -0800

    Flip the remaining assertions

--- test/tender_multipass_test.rb
+++ test/tender_multipass_test.rb
@@ -81,21 +81,21 @@ class TenderMultipassWithOptionsTest < Test::Unit::TestCase
   end
 
   def test_custom_tender_cookie_is_set
-    assert_equal @cookies[:tender_foo], :value => 'bar', :domain => Tender::MultiPass.cookie_domain
+    assert_cookie @cookies[:tender_foo], :value => 'bar', :domain => Tender::MultiPass.cookie_domain
   end
 
   def test_tender_email_cookie_is_set
-    assert_equal @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
+    assert_cookie @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
   end
 
   def test_tender_expires_cookie_is_set
-    assert_equal @cookies[:tender_expires], :value => "1234", :domain => Tender::MultiPass.cookie_domain
+    assert_cookie @cookies[:tender_expires], :value => "1234", :domain => Tender::MultiPass.cookie_domain
   end
 
   def test_tender_hash_cookie_is_set
     digest = OpenSSL::Digest::Digest.new("SHA1")
     hash   = OpenSSL::HMAC.hexdigest(digest, Tender::MultiPass.site_key, "#{Tender::MultiPass.support_domain}/#{@user.email}/1234")
-    assert_equal @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
+    assert_cookie @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
   end
 end
 
@@ -107,20 +107,20 @@ class TenderMultipassWithDefaultOptionsTest < Test::Unit::TestCase
   end
 
   def test_default_tender_cookie_is_set
-    assert_equal @cookies[:tender_bar], :value => 'foo', :domain => Tender::MultiPass.cookie_domain
+    assert_cookie @cookies[:tender_bar], :value => 'foo', :domain => Tender::MultiPass.cookie_domain
   end
 
   def test_tender_email_cookie_is_set
-    assert_equal @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
+    assert_cookie @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
   end
 
   def test_tender_expires_cookie_is_set
-    assert_equal @cookies[:tender_expires], :value => "1234", :domain => Tender::MultiPass.cookie_domain
+    assert_cookie @cookies[:tender_expires], :value => "1234", :domain => Tender::MultiPass.cookie_domain
   end
 
   def test_tender_hash_cookie_is_set
     digest = OpenSSL::Digest::Digest.new("SHA1")
     hash   = OpenSSL::HMAC.hexdigest(digest, Tender::MultiPass.site_key, "#{Tender::MultiPass.support_domain}/#{@user.email}/1234")
-    assert_equal @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
+    assert_cookie @cookies[:tender_email], :value => @user.email, :domain => Tender::MultiPass.cookie_domain
   end
 end
