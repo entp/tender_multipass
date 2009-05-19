eaefb71d02ad396932f50b85579192c70c9a209e
rick <technoweenie@gmail.com>
2009-05-18 17:39:01 -0700

    some docs

--- lib/tender/multi_pass_methods.rb
+++ lib/tender/multi_pass_methods.rb
@@ -1,8 +1,25 @@
 module Tender
   module MultiPassMethods
     module ClassMethods
+      # Define default Tender options for all cookies.  Anything ending in *_url
+      # is changed to a URL when displayed in Tender
+      #
+      #   class User < ActiveRecord::Base
+      #     include Tender::MultiPassMethods
+      #
+      #     tender_multipass do |user|
+      #       {:external_url => "http://myapp.com/admin/users/#{user.id}",
+      #        :plan        => user.plan.name}
+      #     end
+      #   end
+      #
       def tender_multipass(&block)
         block ? @tender_multipass = block : @tender_multipass
+      end
+
+      # Points to the Tender::MultiPass class that implements the cookie format.
+      def tender_multipass_class
+        @tender_multipass_class ||= Tender::MultiPass
       end
     end
 
@@ -10,23 +27,35 @@ module Tender
       base.extend ClassMethods
     end
 
+    # Modifies the given cookie jar with a Tender cookie.  Pass any of the given options
+    # as custom options to add to the cookie.
     def tender_multipass(cookies, options = {})
-      default_options = \
+      if !options.is_a?(Hash)
+        options = {:expires => options}
+      end
+
+      tender_multipass_object.create(cookies, tender_default_options.merge(options))
+    end
+
+    # Expires the Tender cookie using the given cookie jar.
+    def tender_expire(cookies)
+      tender_multipass_object.expire(cookies)
+    end
+
+    # Points to an instance of the Tender::MultiPass object for this record.
+    def tender_multipass_object
+      @tender_multipass_object ||= self.class.tender_multipass_class.new(self)
+    end
+
+    # Points to a hash of the default Tender::MultiPass options.  Call the 
+    # #tender_multipass class method (see above) to change this.
+    def tender_default_options
+      @tender_default_options ||= \
         if multipass = self.class.tender_multipass
           multipass.call(self)
         else
           {}
         end
-
-      if !options.is_a?(Hash)
-        options = {:expires => options}
-      end
-
-      Tender::MultiPass.new(self).create(cookies, default_options.merge(options))
-    end
-
-    def tender_expire(cookies)
-      Tender::MultiPass.new(self).expire(cookies)
     end
   end
 end
